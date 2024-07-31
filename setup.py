54a8678532c892b1ec8f4277efd064708cc21e4a
ravimenta <ravi.menta@lacework.net>
2024-07-31 00:23:25 -0700

    final

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End_to_end_MLOPS_project"
-AUTHOR_USER_NAME = "Somesh"
+AUTHOR_USER_NAME = "Ravi Menta"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "chitranshisomesh@gmail.com"
+AUTHOR_EMAIL = "rmentaik@gmail.com"
 
 
 setuptools.setup(
